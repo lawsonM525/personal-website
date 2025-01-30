6976e3d8f8cd6cfc41321c9a9fbc9d5b0669c839
Michelle L <lawson_m@soshgic.edu.gh>
2025-01-30 00:27:03 -0500

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import { LayoutClient } from "./layout-client"
+import favicon from "@/assets/mia-smile-icon.png"
 
 export const metadata: Metadata = {
   title: "Michelle Lawson",
   description: "Michelle Lawson is a top voice in technology education. With over 150,000 followers across various social media platforms, she has established herself as a leading figure in the field. An accomplished author, Michelle has written multiple books that use fashion and makeup-themed metaphors to make complex computer science concepts more engaging. As an undergraduate researcher, she has published papers with the American Society of Engineering Education, focusing on Computer Science education. Michelle also curates a strong collection of educational resources designed to help students succeed in their STEM degrees.",
+  icons: {
+    icon: favicon.src
+  }
 }
 
 export default function RootLayout({
